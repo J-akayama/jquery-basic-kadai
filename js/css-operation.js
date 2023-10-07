736b3c12a541fa4ff8aa1768222fccd3f9a49b75
J-akayama <ta.200206@gmail.com>
2023-10-07 13:42:57 +0900

    課題提出

---
+++ js/css-operation.js
@@ -0,0 +1,14 @@
+$(function() {
+  // id属性がredの要素がクリックされたら
+  $('#red').on('click', function(){
+    // id属性がtargetの要素のcolorプロパティをredにする
+    $('#target').css('color', 'red');
+  });
+});
+$(function() {
+  // id属性がredの要素がクリックされたら
+  $('#blue').on('click', function(){
+    // id属性がtargetの要素のcolorプロパティをredにする
+    $('#target').css('color', 'blue');
+  });
+});
---
+++ kadai_006/weather.js
@@ -0,0 +1,3 @@
+// headingクラスを追加して、「今日の天気予報」の文字色を赤にする
+// id属性がtargetの要素にclass属性'heading'を追加
+$('#target').addClass('heading');
